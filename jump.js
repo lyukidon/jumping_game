667ea4b86f5db6f66fcc7087f64bd20f7f8941e4
lyukidon <64675543+lyukidon@users.noreply.github.com>
2020-08-17 01:26:50 +0900

    changelog

--- jump.js
+++ jump.js
@@ -1,7 +1,14 @@
+let myGamePiece;
+let enemyOne;
+
+// 게임 시작
 function startGame() {
     myGameArea.start();
+    myGamePiece = new MainPlayer(30, 30, "red", 0, 0);
+    enemyOne = new Enemies(10, 15, 50, 0, 2 * Math.PI)
 }
 
+// 게임 화면
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -10,4 +17,31 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+}
+
+
+// 플레이어 그리기
+function MainPlayer(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function Enemies(radius, color, angleS, angleE, x, y) {
+    this.radius = radius;
+    this.color = color;
+    this.angleS = angleS;
+    this.angleE = angleE;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.angleS, this.angleE, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
 }
